1eb0031b024107996f7c00146f5425d132c0f456
Alex Pletta <alexpletta@gmail.com>
2022-12-03 16:09:57 -0500

    transfer work from non-ag repo

---
+++ viz_euroc_results.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+  traj_filename = 'f_dataset-eurocMH01_mono.txt'
+  traj_data = np.loadtxt(traj_filename)
+
+  keypts_filename = 'kf_dataset-eurocMH01_mono.txt'
+  keypts_data = np.loadtxt(keypts_filename)
+
+  ax = plt.axes(projection='3d')
+  xdata_keypts = keypts_data[:,1]
+  ydata_keypts = keypts_data[:,2]
+  zdata_keypts = keypts_data[:,3]
+  ax.scatter3D(xdata_keypts, ydata_keypts, zdata_keypts, color='red', s=80)
+
+  xdata = traj_data[:,1]
+  ydata = traj_data[:,2]
+  zdata = traj_data[:,3]
+  ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='ocean')
+
+
+  plt.show()
