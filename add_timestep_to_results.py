50b18a2f24a7ab05dd6b132b78af45db406fce5a
Alex Pletta <alexpletta@gmail.com>
2022-12-12 12:25:15 -0500

    mono rsults

--- add_timestep_to_results.py
+++ add_timestep_to_results.py
@@ -3,12 +3,12 @@ import glob
 
 timestamped = []
 
-results_file = 'f_dataset-CMU_collect1_mono.txt'
-final_results_file = 'CMU_collect1_mono_openvins_est.txt'
+results_file = 'f_dataset-CMU_collect1_mono_closestdt2.txt'
+final_results_file = 'CMU_collect1_mono_closestdt2_openvins_est.txt'
 with open(results_file) as f:
     contents = f.readlines()
 
-timestamps_file = '/Users/alexpletta/SLAM/Project/ORB_SLAM3_ag/Examples/Monocular/CMU_TimeStamps/CMU_site_Oporto_clearing_2022_08_17_collect1_processed1_leftcamera.txt'
+timestamps_file = '/Users/alexpletta/SLAM/Project/ORB_SLAM3_ag/Examples/Stereo/CMU_TimeStamps/CMU_site_Oporto_clearing_2022_08_17_collect1_processed1_stereo_closestdt.txt'
 timestamps = []
 with open(timestamps_file) as f:
     time_names = f.readlines()
